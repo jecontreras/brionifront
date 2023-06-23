f83fbc73a12059f11c69adadf9e1910bb4ecbf3b
jecontreras <joseeduar147@gmail.com>
2023-06-23 03:13:36 -0500

    cambios new

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  url: "https://backbriony-011713f2d1ec.herokuapp.com",
+  urlFront: "https://liamstilos.web.app",
+  URLFILE: "https://lokomproaqui.herokuapp.com",
+};
